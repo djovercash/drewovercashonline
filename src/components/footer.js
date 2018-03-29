47536ba8bcadddfe383e2f0e3127198930f433a3
github username <github email address>
2018-03-29 08:42:45 -0400

    Updated Songs

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <h5>@drewovercash 2018</h5>
       </div>
       <div id="socialPlatforms">
-        <h5><a href="https://www.linkedin.com/in/djovercash/" target="_blank" rel="noopener noreferrer"><img className="logo" src="linkedin_logo.png" alt="LinkedIn" /></a></h5>
+        <h5><a href="https://www.linkedin.com/in/drewovercash/" target="_blank" rel="noopener noreferrer"><img className="logo" src="linkedin_logo.png" alt="LinkedIn" /></a></h5>
         <h5><a href="https://github.com/djovercash" target="_blank" rel="noopener noreferrer"><img className="logo" src="github_logo.png" alt="Github" /></a></h5>
         <h5><a href="https://www.facebook.com/DrewOvercashMusic" target="_blank" rel="noopener noreferrer"><img className="logo" src="facebook_logo.png" alt="Facebook" /></a></h5>
         <h5><a href="https://www.twitter.com/djovercash" target="_blank" rel="noopener noreferrer"><img className="logo" src="twitter_logo.png" alt="Twitter" /></a></h5>
