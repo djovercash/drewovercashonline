19451c2b80d8508b69ce51dd5c1df4676d77335a
github username <github email address>
2018-04-13 10:25:16 -0400

    Added Charities

--- src/components/contact.js
+++ src/components/contact.js
@@ -77,17 +77,35 @@ class Contact extends React.Component{
               <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/><br/>
               <label>Subject:  </label>
               <input type="text" name="subject" value={this.state.subject} onChange={this.handleChange}/><br/>
-            </div>
-            <label>Body</label><br/>
-            <textarea name="body" value={this.state.body} onChange={this.handleChange}/><br/>
-            <input className="contactActionButton" type="submit" value="Submit"/>
+            </div><br/>
+            <label>Body: </label><br/>
+            <textarea rows="20" name="body" value={this.state.body} onChange={this.handleChange}/><br/>
+            <input className="contactActionButton" type="submit" value="SUBMIT"/>
           </form>
         </div>
         <div id="contactDetails">
-          <h1>Download my resume</h1>
-          <a href="https://github.com/djovercash/Resume/raw/master/DOvercash_Resume.pdf" download>
-            <button className="contactActionButton">Download</button>
-          </a>
+          <div>
+            <h1>Download My Resume</h1>
+            <a href="https://github.com/djovercash/Resume/raw/master/DOvercash_Resume.pdf" download>
+              <button className="contactActionButton">DOWNLOAD</button>
+            </a>
+          </div>
+          <div>
+            <h1>Follow Me Online</h1>
+            <div id="socialPlatformsContact">
+              <h5><a href="https://www.linkedin.com/in/drewovercash/" target="_blank" rel="noopener noreferrer"><img className="logo" src="linkedin_logo.png" alt="LinkedIn" /></a></h5>
+              <h5><a href="https://github.com/djovercash" target="_blank" rel="noopener noreferrer"><img className="logo" src="github_logo.png" alt="Github" /></a></h5>
+              <h5><a href="https://www.facebook.com/DrewOvercashMusic" target="_blank" rel="noopener noreferrer"><img className="logo" src="facebook_logo.png" alt="Facebook" /></a></h5>
+              <h5><a href="https://www.twitter.com/djovercash" target="_blank" rel="noopener noreferrer"><img className="logo" src="twitter_logo.png" alt="Twitter" /></a></h5>
+              <h5><a href="https://www.instagram.com/djovercash" target="_blank" rel="noopener noreferrer"><img className="logo" src="instagram_logo.png" alt="Instgram" /></a></h5>
+            </div>
+          </div>
+          <div>
+            <h1>Awesome Charities I Care About</h1>
+            <h3><a href="https://www.stjude.org/" target="_blank" rel="noopener noreferrer">St. Jude Childrens Research Hospital</a></h3>
+            <h3><a href="https://broadwaycares.org/" target="_blank" rel="noopener noreferrer">Broadway Cares/Equity Fights Aids</a></h3>
+            <h3><a href="https://www.aclu.org/" target="_blank" rel="noopener noreferrer">ACLU</a></h3>
+          </div>
         </div>
       </div>
     )
