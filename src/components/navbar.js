b0d0c4ea8c813a653a37d13c870ef20e9428300b
github username <github email address>
2018-04-18 14:05:20 -0400

    Added reactive Navbar for Mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,10 +7,16 @@ const Navbar = () => {
       <div id="navbarLogo">
         <h1><NavLink to='/home' exact>DO</NavLink></h1>
       </div>
-      <div id="navbarNavigation">
+      <div id="navbarNavigationDesktop">
         <NavLink to='/web_development' exact>DO Web Development</NavLink>
         <NavLink to='/musical_theatre' exact>DO Musical Theatre</NavLink>
         <NavLink to='/contact' exact>DO Contact</NavLink>
+      </div>
+      <div id="navbarNavigationMobile">
+        <NavLink to='/home' exact><h1>DO</h1></NavLink>
+        <NavLink to="/web_development" exact><img className="mobileNavbarIcon" src="WebMobile.png" alt="Web Development" /></NavLink>
+        <NavLink to="/musical_theatre" exact><img className="mobileNavbarIcon" src="MusicMobile.png" alt="Musical Theatre" /></NavLink>
+        <NavLink to="/contact" exact><img className="mobileNavbarIcon" src="ContactMobile.png" alt="Contact" /></NavLink>
       </div>
     </div>
   )
